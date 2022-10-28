0dd7be347172cd272fcefb643dd4f3619a75b967
백채연 <codusl100@naver.com>
2022-10-29 00:18:47 +0900

    [22.10.29] Upload source file

---
+++ database.py
@@ -0,0 +1,68 @@
+import pymysql
+import sqlite3
+import os
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, 'data')
+
+
+class Database(object):
+    path = os.path.join(data_dir, 'hiScores.db')
+    numScores = 15
+
+    @staticmethod
+    def getSound(music=False): # 사운드를 데이터베이스에서 불러오는 함수
+        conn = sqlite3.connect(Database.path)
+
+        c = conn.cursor()
+        if music:
+            c.execute("CREATE TABLE if not exists music (setting integer)")
+            c.execute("SELECT * FROM music")
+        else:
+            c.execute("CREATE TABLE if not exists sound (setting integer)")
+            c.execute("SELECT * FROM sound")
+        setting = c.fetchall()
+        conn.close()
+        return bool(setting[0][0]) if len(setting) > 0 else False
+
+    @staticmethod
+    def setSound(setting, music=False): # 사운드를 설정하는 함수
+        conn = sqlite3.connect(Database.path)
+
+        c = conn.cursor()
+        if music:
+            c.execute("DELETE FROM music")
+            c.execute("INSERT INTO music VALUES (?)", (setting,))
+        else:
+            c.execute("DELETE FROM sound")
+            c.execute("INSERT INTO sound VALUES (?)", (setting,))
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def getScores(): # 점수를 데이터베이스에서 불러오는 함수
+        conn = pymysql.connect(host='database-batong.cuwmiry5hdel.ap-northeast-2.rds.amazonaws.com', user='admin',
+                               password='batong1234', db='hiScores', charset='utf8')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE if not exists scores
+                     (name text, score integer, accuracy real)''')
+        c.execute("SELECT * FROM scores ORDER BY score DESC")
+        hiScores = c.fetchall()
+        conn.close()
+        return hiScores
+
+    @staticmethod
+    def setScore(hiScores, entry): # 점수를 데이터베이스에 저장하는 함수
+        conn = pymysql.connect(host='database-batong.cuwmiry5hdel.ap-northeast-2.rds.amazonaws.com', user='admin',
+                               password='batong1234', db='hiScores', charset='utf8')
+        c = conn.cursor()
+        if len(hiScores) == Database.numScores:
+            lowScoreName = hiScores[-1][0]
+            lowScore = hiScores[-1][1]
+            c.execute("DELETE FROM scores WHERE (name = %s AND score = %s)",
+                      (lowScoreName, lowScore))
+        c.execute("INSERT INTO scores VALUES (%s,%s,%s)", entry)
+        conn.commit()
+        conn.close()
+
+
---
+++ sprites.py
@@ -0,0 +1,360 @@
+import pygame
+import random
+import math
+from load import load_image
+
+### pygame.sprite.Sprite 자식 클래스 
+### -> MasterSprite, Bomb 
+freq = 1 / 20
+siney_move = 3
+roundy_move = 2
+explosion_linger = 12
+spikey_slope = range(-3, 4)
+spikey_interval = 4
+spikey_period = range(10, 41)
+fasty_movefunc = 3
+
+scr_size = 500
+
+class size :
+    update = scr_size*0.008
+    radius = scr_size*0.04
+    middle = scr_size // 2
+    speed = scr_size*0.002
+    masterspritespeed = scr_size*0.004
+    lives = scr_size*0.06
+    crawly = scr_size*0.006
+    x_background = scr_size*1
+    right = x_background*0.6
+
+def get_size() :
+    global user_size, level_size, scr_size
+    size.update = scr_size*0.008
+    size.radius = scr_size*0.04
+    size.middle = scr_size // 2
+    size.speed = scr_size*0.002
+    size.masterspritespeed = scr_size*0.004
+    size.lives = scr_size*0.06
+    size.crawly = scr_size*0.006
+
+    size.x_background = scr_size*2
+    size.right = size.x_background*0.6
+
+class MasterSprite(pygame.sprite.Sprite):
+    ### 자식 클래스들 : Explosion, Missile, Powerup(<- BombPowerup, ShieldPowerup),
+    ###              Ship, Alien 
+    allsprites = None
+    speed = None
+
+
+class Explosion(MasterSprite): 
+    pool = pygame.sprite.Group()
+    active = pygame.sprite.Group()
+
+    def __init__(self):
+        super().__init__()
+        self.image, self.rect = load_image('explosion.png', -1)
+        self.linger = MasterSprite.speed * 3
+
+    @classmethod
+    def position(cls, loc):
+        if len(cls.pool) > 0:
+            explosion = cls.pool.sprites()[0]
+            explosion.add(cls.active, cls.allsprites)
+            explosion.remove(cls.pool)
+            explosion.rect.center = loc
+            explosion.linger = 12
+
+    def update(self):
+        self.linger -= 1
+        if self.linger <= 0:
+            self.remove(self.allsprites, self.active)
+            self.add(self.pool)
+
+
+class Missile(MasterSprite):
+    pool = pygame.sprite.Group()
+    active = pygame.sprite.Group()
+
+    def __init__(self):
+        super().__init__()
+        self.image, self.rect = load_image('missile.png', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+
+    @classmethod
+    def position(cls, loc):
+        if len(cls.pool) > 0:
+            missile = cls.pool.sprites()[0]
+            missile.add(cls.allsprites, cls.active)
+            missile.remove(cls.pool)
+            missile.rect.midbottom = loc
+
+    def table(self):
+        self.add(self.pool)
+        self.remove(self.allsprites, self.active)
+
+    def update(self):
+        newpos = self.rect.move(0, -4 * MasterSprite.speed)
+        self.rect = newpos
+        if self.rect.top < self.area.top:
+            self.table()
+
+
+class Bomb(pygame.sprite.Sprite):
+    ### 폭탄이 터질때
+    def __init__(self, ship):
+        super().__init__()
+        self.image = None
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.radius = 20
+        self.radiusIncrement = 4
+        self.rect = ship.rect
+
+    def update(self):
+        self.radius += self.radiusIncrement
+        pygame.draw.circle(
+            pygame.display.get_surface(),
+            pygame.Color(0, 0, 255, 128),
+            self.rect.center, self.radius, 3)
+        if (self.rect.center[1] - self.radius <= self.area.top
+            and self.rect.center[1] + self.radius >= self.area.bottom
+            and self.rect.center[0] - self.radius <= self.area.left
+                and self.rect.center[0] + self.radius >= self.area.right):
+            self.kill()
+
+class Coin(MasterSprite):
+    def __init__(self):
+        super().__init__()
+        self.image, self.rect = load_image('coin.png', -1)
+        self.original = self.image
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.midtop = (random.randint(
+                            self.area.left + self.rect.width // 2,
+                            self.area.right - self.rect.width // 2),
+                            self.area.top)
+    def update(self):
+        center = self.rect.center
+        self.rect = self.image.get_rect(
+            center=(
+                center[0],
+                center[1] +
+                MasterSprite.speed))
+
+class Powerup(MasterSprite):
+    ## 아이템 획득
+    def __init__(self, kindof):
+        super().__init__()
+        self.image, self.rect = load_image(kindof + '_powerup.png', -1)
+        self.original = self.image
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        ### random한 위치에 드롭
+        self.rect.midtop = (random.randint(
+                            self.area.left + self.rect.width // 2,
+                            self.area.right - self.rect.width // 2),
+                            self.area.top)
+        self.angle = 0
+
+    def update(self):
+        center = self.rect.center
+        self.angle = (self.angle + 2) % 360
+        rotate = pygame.transform.rotate
+        self.image = rotate(self.original, self.angle)
+        self.rect = self.image.get_rect(
+            center=(
+                center[0],
+                center[1] +
+                MasterSprite.speed))
+
+
+class BombPowerup(Powerup):
+    def __init__(self):
+        super().__init__('bomb')
+        self.pType = 'bomb'
+
+
+class ShieldPowerup(Powerup):
+    def __init__(self):
+        super().__init__('shield')
+        self.pType = 'shield'
+
+class HalfPowerup(Powerup):
+    def __init__(self):
+        super().__init__('half')
+        self.pType = 'half'
+        self.image = pygame.transform.scale(self.image, (50,30))
+
+class Ship(MasterSprite):
+    def __init__(self):
+        super().__init__()
+        self.image, self.rect = load_image('ship.png', -1)
+        self.original = self.image
+        self.shield, self.rect = load_image('ship_shield.png', -1)
+        self.screen = pygame.display.get_surface()
+        self.area = self.screen.get_rect()
+        self.rect.midbottom = (self.screen.get_width() // 2, self.area.bottom)
+        self.radius = max(self.rect.width, self.rect.height)
+        self.alive = True  ##### life
+        self.shieldUp = False
+        self.vert = 0
+        self.horiz = 0
+
+    def initializeKeys(self):
+        keyState = pygame.key.get_pressed()
+        self.vert = 0
+        self.horiz = 0
+        if keyState[pygame.K_w]:
+            self.vert -= 2 * MasterSprite.speed
+        if keyState[pygame.K_a]:
+            self.horiz -= 2 * MasterSprite.speed
+        if keyState[pygame.K_s]:
+            self.vert += 2 * MasterSprite.speed
+        if keyState[pygame.K_d]:
+            self.horiz += 2 * MasterSprite.speed
+
+    def update(self):
+        newpos = self.rect.move((self.horiz, self.vert))
+        newhoriz = self.rect.move((self.horiz, 0))
+        newvert = self.rect.move((0, self.vert))
+
+        if not (newpos.left <= self.area.left
+                or newpos.top <= self.area.top
+                or newpos.right >= self.area.right
+                or newpos.bottom >= self.area.bottom):
+            self.rect = newpos
+        elif not (newhoriz.left <= self.area.left
+                  or newhoriz.right >= self.area.right):
+            self.rect = newhoriz
+        elif not (newvert.top <= self.area.top
+                  or newvert.bottom >= self.area.bottom):
+            self.rect = newvert
+
+        if self.shieldUp and self.image != self.shield:
+            self.image = self.shield
+
+        if not self.shieldUp and self.image != self.original:
+            self.image = self.original
+
+    def bomb(self):
+        return Bomb(self)
+
+
+class Alien(MasterSprite):
+    ##Siney : green, Roundy : red, Spikey : blue, Fasty : white, Crawly : yellow
+    pool = pygame.sprite.Group()
+    active = pygame.sprite.Group()
+
+    def __init__(self, color):
+        super().__init__()
+        self.image, self.rect = load_image(
+            'space_invader_' + color + '.png', -1)
+        self.initialRect = self.rect
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.loc = 0
+        self.radius = min(self.rect.width // 2, self.rect.height // 2)
+
+    def update_background(self) :
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        
+    @classmethod
+    def position(cls):  
+        if len(cls.pool) > 0 and cls.numOffScreen > 0:
+            alien = random.choice(cls.pool.sprites())
+            if isinstance(alien, Crawly):
+                alien.rect.midbottom = (random.choice(
+                    (alien.area.left, alien.area.right)),
+                    random.randint(
+                    (alien.area.bottom * 3) // 4,
+                    alien.area.bottom))
+            else:
+                alien.rect.midtop = (random.randint(
+                    alien.area.left
+                    + alien.rect.width // 2,
+                    alien.area.right
+                    - alien.rect.width // 2),
+                    alien.area.top)
+            alien.initialRect = alien.rect
+            alien.loc = 0
+            alien.add(cls.allsprites, cls.active)
+            alien.remove(cls.pool)
+            Alien.numOffScreen -= 1
+
+    def update(self):
+        horiz, vert = self.moveFunc()
+        if horiz + self.initialRect.x > scr_size:
+            horiz -= scr_size + self.rect.width
+        elif horiz + self.initialRect.x < 0 - self.rect.width:
+            horiz += scr_size + self.rect.width
+        self.rect = self.initialRect.move((horiz, self.loc + vert))
+        self.loc = self.loc + MasterSprite.speed * size.speed
+        if self.rect.top > self.area.bottom:
+            self.table()
+            Alien.numOffScreen += 1
+
+    def table(self):
+        self.kill()
+        self.add(self.pool)
+   
+
+class Siney(Alien):
+    def __init__(self):
+        super().__init__('green')
+        self.amp = random.randint(self.rect.width, siney_move * self.rect.width)
+        self.freq = freq
+        self.moveFunc = lambda: (self.amp * math.sin(self.loc * self.freq), 0)
+
+
+class Roundy(Alien):
+    def __init__(self):
+        super().__init__('red')
+        self.amp = random.randint(self.rect.width, roundy_move * self.rect.width)
+        self.freq = freq
+        self.moveFunc = lambda: (
+            self.amp *
+            math.sin(
+                self.loc *
+                self.freq),
+            self.amp *
+            math.cos(
+                self.loc *
+                self.freq))
+
+
+class Spikey(Alien):
+    def __init__(self):
+        super().__init__('blue')
+        self.slope = random.choice(list(x for x in spikey_slope if x != 0))
+        self.period = random.choice(list(spikey_interval * x for x in range(10, 41)))
+        self.moveFunc = lambda: (self.slope * (self.loc % self.period)
+                                 if self.loc % self.period < self.period // 2
+                                 else self.slope * self.period // 2
+                                 - self.slope * ((self.loc % self.period)
+                                 - self.period // 2), 0)
+
+
+class Fasty(Alien):
+    def __init__(self):
+        super().__init__('white')
+        self.moveFunc = lambda: (0, fasty_movefunc * self.loc)
+
+
+class Crawly(Alien):
+    def __init__(self):
+        super().__init__('yellow')
+        self.moveFunc = lambda: (self.loc, 0)
+
+    def update(self):
+        horiz, vert = self.moveFunc()
+        horiz = (-horiz if self.initialRect.center[0] == self.area.right
+                 else horiz)
+        if (horiz + self.initialRect.left > self.area.right
+                or horiz + self.initialRect.right < self.area.left):
+            self.table()
+            Alien.numOffScreen += 1
+        self.rect = self.initialRect.move((horiz, vert))
+        self.loc = self.loc + MasterSprite.speed
