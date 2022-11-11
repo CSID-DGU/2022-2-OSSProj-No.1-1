aaba084dd0dee5eced46ec066bb44d8414f8decf
백채연 <codusl100@naver.com>
2022-11-11 12:40:23 +0900

    [22.11.11] Change variable & class name

--- sprites.py
+++ sprites.py
@@ -4,46 +4,7 @@ import math
 from load import load_image
 from pygame.locals import *
 
-### pygame.sprite.Sprite 자식 클래스 
-### -> MasterSprite, Bomb 
-freq = 1 / 20
-siney_move = 3
-roundy_move = 2
-explosion_linger = 12
-spikey_slope = range(-3, 4)
-spikey_interval = 4
-spikey_period = range(10, 41)
-fasty_movefunc = 3
-
-scr_size = 500
-
-class size :
-    update = scr_size*0.008
-    radius = scr_size*0.04
-    middle = scr_size // 2
-    speed = scr_size*0.002
-    masterspritespeed = scr_size*0.004
-    lives = scr_size*0.06
-    crawly = scr_size*0.006
-    x_background = scr_size*1
-    right = x_background*0.6
-
-def get_size() :
-    global user_size, level_size, scr_size
-    size.update = scr_size*0.008
-    size.radius = scr_size*0.04
-    size.middle = scr_size // 2
-    size.speed = scr_size*0.002
-    size.masterspritespeed = scr_size*0.004
-    size.lives = scr_size*0.06
-    size.crawly = scr_size*0.006
-
-    size.x_background = scr_size*2
-    size.right = size.x_background*0.6
-
 class MasterSprite(pygame.sprite.Sprite):
-    ### 자식 클래스들 : Explosion, Missile, Powerup(<- BombPowerup, ShieldPowerup),
-    ###              Ship, Alien 
     allsprites = None
     speed = None
 
@@ -73,14 +34,14 @@ class Explosion(MasterSprite):
             self.add(self.pool)
         
         
-########### kirin 추가
-class Kirin(MasterSprite):
+## Player 추가
+class Player(MasterSprite):
     def __init__(self, screen_size):
         super().__init__()
-        self.image, self.rect = load_image('kirin.png', -1)
+        self.image, self.rect = load_image('kirin.png', -1) # 캐릭터 이미지
         self.original = self.image
-        self.shield, self.rect = load_image('kirin_shield.png', -1)
-        self.fart, self.rect = load_image('kirin_bomb.png', -1)
+        self.shield, self.rect = load_image('kirin_shield.png', -1) # 쉴드 상태 캐릭터 이미지
+        self.fart, self.rect = load_image('kirin_bomb.png', -1) # 폭탄 상태 캐릭터 이미지
         self.screen_size = screen_size
         self.ratio = (self.screen_size / 400) # 비율 변동
         self.screen = pygame.display.set_mode((self.screen_size, self.screen_size), HWSURFACE|DOUBLEBUF|RESIZABLE)
@@ -138,7 +99,7 @@ class Kirin(MasterSprite):
     def bomb(self):
         return Bomb(self)
 
-class Friendkirin(MasterSprite):
+class FriendPlayer(MasterSprite):
     def __init__(self, screen_size):
         super().__init__()
         self.image, self.rect = load_image('friendkirin.png', -1)
@@ -152,7 +113,7 @@ class Friendkirin(MasterSprite):
     def remove(self) :
         pygame.sprite.Sprite.kill(self)
 
-class Kirin2(MasterSprite):
+class Player2(MasterSprite):
     def __init__(self, screen_size):
         super().__init__()
         self.image, self.rect = load_image('ship.png', -1)
@@ -202,7 +163,7 @@ class Kirin2(MasterSprite):
     def bomb(self):
         return Bomb(self)
 
-class Kirin3(MasterSprite):
+class Player3(MasterSprite):
     def __init__(self, screen_size):
         super().__init__()
         self.image, self.rect = load_image('ship.png', -1)
@@ -252,14 +213,14 @@ class Kirin3(MasterSprite):
     def bomb(self):
         return Bomb(self)
 
-class Bear(MasterSprite):
+class Monster(MasterSprite):
     pool = pygame.sprite.Group()
     active = pygame.sprite.Group()
 
     def __init__(self, trait, screen_size):
         super().__init__()
         self.image, self.rect = load_image(
-            'bear_' + trait + '.png', -1)
+            trait + '.png', -1)
         self.initialRect = self.rect
         self.screen_size = screen_size
         self.ratio = (self.screen_size / 500)
@@ -271,25 +232,25 @@ class Bear(MasterSprite):
     @classmethod
     def position(cls):
         if len(cls.pool) > 0 and cls.numOffScreen > 0:
-            bear = random.choice(cls.pool.sprites())
-            if isinstance(bear, Panda):
-                bear.rect.midbottom = (random.choice(
-                    (bear.area.left, bear.area.right)),
+            monster = random.choice(cls.pool.sprites())
+            if isinstance(monster, Blue):
+                monster.rect.midbottom = (random.choice(
+                    (monster.area.left, monster.area.right)),
                     random.randint(
-                    (bear.area.bottom * 3) // 4,
-                    bear.area.bottom))
+                    (monster.area.bottom * 3) // 4,
+                    monster.area.bottom))
             else:
-                bear.rect.midtop = (random.randint(
-                    bear.area.left
-                    + bear.rect.width // 2,
-                    bear.area.right
-                    - bear.rect.width // 2),
-                    bear.area.top)
-            bear.initialRect = bear.rect
-            bear.loc = 0
-            bear.add(cls.allsprites, cls.active)
-            bear.remove(cls.pool)
-            Bear.numOffScreen -= 1
+                monster.rect.midtop = (random.randint(
+                    monster.area.left
+                    + monster.rect.width // 2,
+                    monster.area.right
+                    - monster.rect.width // 2),
+                    monster.area.top)
+            monster.initialRect = monster.rect
+            monster.loc = 0
+            monster.add(cls.allsprites, cls.active)
+            monster.remove(cls.pool)
+            monster.numOffScreen -= 1
 
     def update(self, screen_size):
         self.screen_size = screen_size
@@ -302,14 +263,14 @@ class Bear(MasterSprite):
         self.loc = self.loc + MasterSprite.speed
         if self.rect.top > self.area.bottom:
             self.table()
-            Bear.numOffScreen += 1
+            Monster.numOffScreen += 1
 
     def table(self):
         self.kill()
         self.add(self.pool)
 
 
-class Green(Bear):
+class Green(Monster):
     def __init__(self, screen_size):
         super().__init__('green',screen_size)
         self.amp = random.randint(self.rect.width, 3.5 * self.rect.width) ## 적 좌우 움직임 변동
@@ -318,9 +279,9 @@ class Green(Bear):
         self.pType = 'green'
 
 
-class Sunglasses(Bear):
+class Pink(Monster):
     def __init__(self, screen_size):
-        super().__init__('sunglasses',screen_size)
+        super().__init__('pink',screen_size)
         self.amp = random.randint(self.rect.width, 2 * self.rect.width)
         self.freq = 1 / (25) # 원 움직임 변동 횟수 늘리기
         self.moveFunc = lambda: (
@@ -332,12 +293,12 @@ class Sunglasses(Bear):
             math.cos(
                 self.loc *
                 self.freq))
-        self.pType = 'sunglasses'
+        self.pType = 'pink'
 
 
-class Brown(Bear):
+class Yellow(Monster):
     def __init__(self, screen_size):
-        super().__init__('brown',screen_size)
+        super().__init__('yellow',screen_size)
         self.slope = random.choice(list(x for x in range(-3, 3) if x != 0)) # 범위 -3 ~ 3이 게임 진행에 있어 안정적
         self.period = random.choice(list(4 * x for x in range(15, 41))) # 기본 적의 좌우 움직이는 주기 start를 늘림
         self.moveFunc = lambda: (self.slope * (self.loc % self.period)
@@ -345,17 +306,17 @@ class Brown(Bear):
                                  else self.slope * self.period // 2
                                  - self.slope * ((self.loc % self.period)
                                  - self.period // 2), 0)
-        self.pType = 'brown'
+        self.pType = 'yellow'
 
 
-class Stone(Bear):
+class Grey(Monster):
     def __init__(self, screen_size):
         super().__init__('stone',screen_size)
         self.moveFunc = lambda: (0, 1.5 * self.loc)
         self.pType = 'stone'
 
 
-class Panda(Bear):
+class Blue(Monster):
     def __init__(self, screen_size):
         super().__init__('panda',screen_size)
         self.moveFunc = lambda: (self.loc, 0)
@@ -368,7 +329,7 @@ class Panda(Bear):
         if (horiz + self.initialRect.left > self.area.right
                 or horiz + self.initialRect.right < self.area.left):
             self.table()
-            Bear.numOffScreen += 1
+            Monster.numOffScreen += 1
         self.rect = self.initialRect.move((horiz, vert))
         self.loc = self.loc + MasterSprite.speed
         
@@ -434,14 +395,14 @@ class Leaf(MasterSprite):
 
 
 class Bomb(pygame.sprite.Sprite):
-    def __init__(self, kirin):
+    def __init__(self, Player):
         super().__init__()
         self.image = None
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.radius = 20
         self.radiusIncrement = 4
-        self.rect = kirin.rect
+        self.rect = Player.rect
 
     def update(self):
         self.radius += self.radiusIncrement
@@ -503,7 +464,7 @@ class DoubleleafPowerup(Powerup):
 class FriendPowerup(Powerup):
     def __init__(self, screen_size):
         super().__init__('friendkirin', screen_size)
-        self.pType = 'friendkirin'
+        self.pType = 'friendPlayer'
 
 class LifePowerup(Powerup):
     def __init__(self, screen_size):
